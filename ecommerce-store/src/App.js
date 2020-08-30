5a72b5d5cc7b6a0a3e66314a52523b215e8e5cf5
Shefali Bisht <shefali.bisht@nagarro.com>
2020-08-30 14:52:58 +0530

    Added a header with a svg logo

--- ecommerce-store/src/App.js
+++ ecommerce-store/src/App.js
@@ -4,10 +4,11 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/home-page/Homepage.component';
 import ShopPage from './pages/shop-page/shop.component'
-
+import Header from './components/header/header.component';
 function App() {
   return (
     <div>
+      <Header />
    <Switch>
      <Route exact path='/' component={HomePage} />
      <Route exact path='/shop' component={ShopPage} />
---
+++ ecommerce-store/src/components/header/header.component.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import '../../assets/site-logo.svg'
+import {ReactComponent as Logo} from '../../assets/site-logo.svg';
+import { Link } from 'react-router-dom';
+import './header.component.style.scss';
+
+
+const Header = () => (
+    <div className='header'>
+      <Link className='logo-container' to='/'>
+        <Logo className='logo' />
+      </Link>
+      <div className='options'>
+        <Link className='option' to='/shop'>
+          SHOP
+        </Link>
+        <Link className='option' to='/shop'>
+          CONTACT
+        </Link>
+      </div>
+    </div>
+  );
+  
+
+
+export default Header;
