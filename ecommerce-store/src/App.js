c5994cabeb7116d90d22874ecb433de933405c44
Shefali Bisht <shefali.bisht@nagarro.com>
2020-08-30 10:59:30 +0530

    created react ecommerce app template phase1

---
+++ ecommerce-store/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import HomePage from './components/Homepage.component';
+
+function App() {
+  return (
+   // <div className="App">
+    
+      <HomePage />
+   // </div>
+  );
+}
+
+export default App;
---
+++ ecommerce-store/src/components/Homepage.component.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './Homepage.style.scss';
+
+
+const HomePage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                <h1 className='title'>Bags</h1>
+                <span className='sub-title'>Handbags</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                <h1 className='title'>Bags</h1>
+                <span className='sub-title'>Handbags</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                <h1 className='title'>Bags</h1>
+                <span className='sub-title'>Handbags</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                <h1 className='title'>Bags</h1>
+                <span className='subtitle'>Handbags</span>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default HomePage;
