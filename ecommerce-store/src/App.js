efdfecfe9cf35f53883cd4d5cda51157e98fd9f0
Shefali Bisht <shefali.bisht@nagarro.com>
2020-08-30 12:05:00 +0530

    Made Menu-items cataloge and added styling

--- ecommerce-store/src/App.js
+++ ecommerce-store/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
-import HomePage from './components/Homepage.component';
+import HomePage from './pages/Homepage.component';
 
 function App() {
   return (
---
+++ ecommerce-store/src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './menu-item.style.scss'
+
+
+const MenuItem = ({ title, size, imgurl }) => (
+    <div   className={`${size} menu-item`}>
+        <div className='background-image' style={{ background: `url(${imgurl})` }} />
+
+        <div className='content'>
+            <h1 className='title'>
+                {title.toUpperCase()}
+            </h1>
+            <span className='subtitle'> Buy now! </span>
+        </div>
+    </div>
+);
+
+export default MenuItem;
