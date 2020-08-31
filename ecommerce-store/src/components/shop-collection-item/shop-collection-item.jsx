3b1e7729fae035208d8a973b312fc84b851ebb38
Shefali Bisht <shefali.bisht@nagarro.com>
2020-08-31 17:53:49 +0530

    Adding product to cart dropdown phase2

--- ecommerce-store/src/components/shop-collection-item/shop-collection-item.jsx
+++ ecommerce-store/src/components/shop-collection-item/shop-collection-item.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { connect } from 'react-redux';
+
+import CustomButton from '../custom-button/custom-button.component';
+import { addItem } from '../../redux/cart/cart.actions';
 
 import './shop-collection-item.style.scss'
 
-const ShopCollectionItem = ({ id, name, price, imageUrl }) => (
+const ShopCollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item;
+
+  return (
     <div className='collection-item'>
       <div
         className='image'
@@ -14,8 +21,19 @@ const ShopCollectionItem = ({ id, name, price, imageUrl }) => (
         <span className='name'>{name}</span>
         <span className='price'>{price}</span>
       </div>
+      <CustomButton onClick={() => addItem(item)} inverted>
+        Add to cart
+      </CustomButton>
     </div>
   );
+};
 
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addItem(item))
+});
 
-export default ShopCollectionItem;
+export default connect(
+  null,
+  mapDispatchToProps
+)(ShopCollectionItem);
+
--- ecommerce-store/src/components/shop-collection/shop-collection.jsx
+++ ecommerce-store/src/components/shop-collection/shop-collection.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 
 import ShopCollectionItem from '../shop-collection-item/shop-collection-item';
+
+
 import './shop-collection.style.scss';
 
 const ShopCollection = ({ title, items }) => (
   <div className='collection-preview'>
     <h1 className='title'>{title.toUpperCase()}</h1>
     <div className='preview'>
-        {/* Display only 4-5 items in the preview */}
       {items
         .filter((item, idx) => idx < 4)
-        .map(({ id, ...otherItemProps }) => (
-          <ShopCollectionItem key={id} {...otherItemProps} />
+        .map(item => (
+          <ShopCollectionItem key={item.id} item={item} />
         ))}
     </div>
   </div>
